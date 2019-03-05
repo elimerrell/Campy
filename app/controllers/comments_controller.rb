a700d616d9d4415a6da0d28f4593b3676d985f84
elimerrell <elijah.merrell@gmail.com>
2019-03-04 21:27:13 -0800

    added comments to facility show page, flash messages for comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,7 +16,7 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     if @comment.save
       flash[:notice] = "Comment successfully created"
-      redirect_to comment_path(@comment)
+      redirect_to facility_path(@comment.facility)
     else
       flash[:notice] = "Something went wrong, please try again"
     end
@@ -26,5 +26,5 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:user_id, :facility_id, :raing, :title, :comment)
-  end 
+  end
 end
